43a10d5952ea0c1f150723c114e7db5e4e678a1c
hungHoangDang <33661599+hungHoangDang@users.noreply.github.com>
2017-12-08 16:29:24 +0700

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -6,7 +6,7 @@ import { Image } from 'bloomer';
 export default class MovieCard extends Component {
   render() {
     const { title, poster_path, overview, vote_average} = this.props.movie;
-    let imgSrc = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`;
+    let imgSrc = `https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`
     {/* Can I do this?
     let imgSrc = `http://image.tmdb.org/t/p/w342{poster_path}`;
     and why does he need the part of _and_h556_bestv2$ */}
